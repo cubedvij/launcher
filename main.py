9cbc24a03f8d6a2e2c74f741f3ea09ead23a31ac
hampta <art.mr.hampta@gmail.com>
2025-05-09 09:29:37 +0300

    Fix: ensure meipass is copied after clearing old folder in compiled mode

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if _COMPILED:
     logging.getLogger("flet_core").setLevel(logging.INFO)
     # HACK: remove old meipass folder
     updater.clear_old_meipass()
+    updater.copy_meipass()
 else:
     logging.basicConfig(
         level=logging.INFO,
