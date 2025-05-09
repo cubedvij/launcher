7d08e8aa4bb4517f651a8e32b605ae3e6105550d
hampta <art.mr.hampta@gmail.com>
2025-05-09 08:11:17 +0300

    Remove duplicate project installation step and streamline version assignment logic in update_version.py

--- update_version.py
+++ update_version.py
@@ -3,11 +3,10 @@ import sys
 import subprocess
 
 git_commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], text=True)
-version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"], text=True).strip()
 if sys.argv[1] == "--release":
-    new_version = version
+    new_version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"], text=True).strip()
 elif sys.argv[1] == "--dev":
-    new_version = f"{version}-dev-{git_commit.strip()}"
+    new_version = f"dev-{git_commit.strip()}"
 else:
     print("Unknown argument, use --release or --dev")
     sys.exit(1)
