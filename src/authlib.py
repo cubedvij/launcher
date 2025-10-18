870a5b35b4b80e80c71806d1b7b5ce8f57929ee8
hampta <art.mr.hampta@gmail.com>
2025-10-19 02:24:34 +0300

    improve authlib-injector download

--- src/authlib.py
+++ src/authlib.py
@@ -1,6 +1,8 @@
+import os
 import httpx
 import hashlib
 import logging
+from zipfile import ZipFile
 
 from minecraft_launcher_lib._helper import empty
 
@@ -9,7 +11,7 @@ from minecraft_launcher_lib._helper import empty
 class Authlib:
     def __init__(self):
         self.base_url = "https://api.github.com/repos/yushijinhun/authlib-injector/releases"
-        self.releases = []
+        self.releases = self.get_releases()
     
     def get_releases(self):
         response = httpx.get(self.base_url)
@@ -18,20 +20,17 @@ class Authlib:
         self.releases = response.json()
         return self.releases
 
-    def get_latest_release(self):
-        if not self.releases:
-            self.get_releases()
-        return self.releases[0]
-
-    def download_latest_release(self, path, callback) -> bool:
+    def download_latest_release(self, path, callback: dict) -> bool:
         """Download the latest release asset from GitHub with retries and chunked download."""
 
         max_retries = 3
+        current_version = self.get_authlib_version(path) if os.path.exists(path) else None
+        if current_version == self.get_latest_release_version():
+            logging.info("Authlib-injector is already up to date.")
+            return True
         for attempt in range(max_retries):
             try:
-                if not self.releases:
-                    self.get_releases()
-                latest = self.get_latest_release()
+                latest = self.releases[0]
                 asset = latest["assets"][0]
                 url = asset["browser_download_url"]
                 with httpx.stream("GET", url, follow_redirects=True, timeout=60) as response:
@@ -54,9 +53,10 @@ class Authlib:
         return False
     
     def get_latest_release_hash(self):
-        if not self.releases:
-            self.get_releases()
-        return self.get_latest_release()["tag_name"]
+        return self.releases[0]["assets"][0]["digest"]
+    
+    def get_latest_release_version(self):
+        return self.releases[0]["tag_name"].replace("v", "")
     
     def check_authlib(self, path):
         with open(path, "rb") as f:
@@ -65,5 +65,13 @@ class Authlib:
         latest_hash = self.get_latest_release_hash()
         return installed_hash == latest_hash
 
-        
+    def get_authlib_version(self, path):
+        with ZipFile(path, 'r') as zip_ref:
+            with zip_ref.open("META-INF/MANIFEST.MF") as manifest_file:
+                for line in manifest_file:
+                    decoded_line = line.decode('utf-8').strip()
+                    if decoded_line.startswith("Implementation-Version:"):
+                        return decoded_line.split(":", 1)[1].strip()
+        return None
+
 authlib = Authlib()
